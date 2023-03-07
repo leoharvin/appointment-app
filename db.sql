228766b67e3701a7eb3b8a0aa15dad6013a972e3
Leo Harvin Niccols <leoharvinniccols@gmail.com>
2023-03-07 17:57:01 +0530

    database

---
+++ db.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 07, 2023 at 12:22 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appointment`
+--
+CREATE DATABASE IF NOT EXISTS `appointment` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `appointment`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+DROP TABLE IF EXISTS `bookings`;
+CREATE TABLE IF NOT EXISTS `bookings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `slot` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `name`, `user_id`, `slot`, `date`) VALUES
+(1, 'Leo', 1, '10 AM', '2023-03-07'),
+(2, 'Ajulraj', 1, '11 AM', '2023-03-07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_tokens`
+--
+
+DROP TABLE IF EXISTS `login_tokens`;
+CREATE TABLE IF NOT EXISTS `login_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(300) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login_tokens`
+--
+
+INSERT INTO `login_tokens` (`id`, `token`, `user_id`) VALUES
+(1, '1c2c646dea24e36648d90b07610341e223f1ad8e2d942f8298151d5cc111cdb3710424aa8ae4c903e273ee29faa6429440a5f8800a2c8c2780e674e95aadd58d', 1),
+(2, '25c7688bf703badb91ed6357650c4918db378f9f3276596c51d9b0d1579f80f7db2201fbb12ebf6605a01ff7903001d625d28f67e6043920efbb0ea34b5907d3', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `address`, `phone`) VALUES
+(1, 'Leo Harvin Niccols', 'leoharvinniccols@gmail.com', '1234', 'Palackal House', '8089965280');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
